c0514b9527338760265e03131030a990cfcac471
Anthony Hernandez <anthonyh2020@gmail.com>
2020-09-10 03:52:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  def initialize(name, breed = Mutt)
+  def initialize(name, breed = "Mutt")
     @name = name
+    @breed = breed
   end
   def name
     @name
-  end
   end
   def breed
     @breed
